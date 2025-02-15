654003811e4854fe9a63c94810de59b31ce75827
ZweiHenky <ronnhy7@gmail.com>
2025-02-14 18:11:39 -0600

    fix carosule dependency

--- app/(tabs)/settings/index.tsx
+++ app/(tabs)/settings/index.tsx
@@ -1,64 +1,165 @@
-import { View, Text, Pressable, ActivityIndicator, FlatList } from 'react-native';
-import { useEffect } from 'react';
-import { useRouter } from 'expo-router';
-import Icon from "@expo/vector-icons/FontAwesome6";
-import { useAuthStore } from '@/presentation/store/auth/useAuthStore';
-import { MenuOption } from '@/presentation/components/settings/menuOptions';
+import { View, Text, ScrollView, Pressable, ActivityIndicator } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import Icon from '@expo/vector-icons/FontAwesome6'
+import { useAuthStore } from '@/presentation/store/auth/useAuthStore'
+import { router } from 'expo-router'
 
+export default function index() {
 
-
-export default function Index() {
-  const { logOut, user, status, checkStatus } = useAuthStore();
-  const router = useRouter();
+  const {logOut, user, status, checkStatus} = useAuthStore()
 
   useEffect(() => {
-    checkStatus();
-  }, []);
+    checkStatus()
+  }, [])
+  
+    if (status === 'checking') {
+      return (
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginBottom: 5,
+          }}
+        >
+          <ActivityIndicator />
+        </View>
+      );
+    }
 
-  if (status === 'checking') {
-    return (
-      <View className='flex-1 justify-center items-center'>
-        <ActivityIndicator />
-      </View>
-    );
-  }
+      if (status === 'unauthenticated') {
+        // Guardar la ruta del usuario
+        return (
+          <View className='flex-1'>
+          <ScrollView className='flex-1'>
+            <View className='bg-white w-[90%] gap-4 mx-auto mt-5 rounded-xl'>
+    
+              <Pressable className='flex-row w-full mt-5 items-center p-5 mx-auto bg-white justify-between rounded-xl'>
+                <View className='flex-row gap-3 items-center'>
+                  <Icon name='credit-card' size={18} color='black' />
+                  <Text className='text-lg'>Plan actual: Gratuito</Text>
+                </View>
+                <Icon name='caret-right' size={20} color='black' />
+              </Pressable>
+    
+              <View className='w-[90%] bg-light-primary h-[0.8px] mx-auto' />
+    
+              <Pressable className='flex-row w-full items-center p-5 mx-auto bg-white justify-between rounded-xl'>
+                <View className='flex-row gap-3 items-center'>
+                  <Icon name='moon' size={18} color='black' />
+                  <Text className='text-lg'>Modo Oscuro</Text>
+                </View>
+                <Icon name='caret-right' size={20} color='black' />
+              </Pressable>
+    
+              <View className='w-[90%] bg-light-primary h-[0.8px] mx-auto' />
+    
+              <Pressable className='flex-row w-full items-center p-5 mx-auto bg-white justify-between rounded-xl'>
+                <View className='flex-row gap-3 items-center'>
+                  <Icon name='shield-halved' size={18} color='black' />
+                  <Text className='text-lg'>Politicas de privacidad</Text>
+                </View>
+                <Icon name='caret-right' size={20} color='black' />
+              </Pressable>
+    
+              <View className='w-[90%] bg-light-primary h-[0.8px] mx-auto' />
+    
+              <Pressable className='flex-row w-full items-center p-5 mx-auto bg-white justify-between rounded-xl'>
+                <View className='flex-row gap-3 items-center'>
+                  <Icon name='file-lines' size={18} color='black' />
+                  <Text className='text-lg'>Terminos y condiciones</Text>
+                </View>
+                <Icon name='caret-right' size={20} color='black' />
+              </Pressable>
+    
+              <View className='w-[90%] bg-light-primary h-[0.8px] mx-auto' />
+                <Pressable onPress={ () => router.replace("/auth/login")} className='flex-row w-full items-center mb-3 p-5 mx-auto bg-white justify-between rounded-xl'>
+                  <View className='flex-row gap-3 items-center'>
+                    <Icon name='right-to-bracket' size={18} color='black' />
+                    <Text className='text-lg'>Iniciar Sesion</Text>
+                  </View>
+                  <Icon name='caret-right' size={20} color='black' />
+                </Pressable>
+            </View>
+          </ScrollView>
+        </View>
+        )
+      }
 
-  const options = [
-    { icon: 'credit-card', label: 'Plan actual: Gratuito' },
-    { icon: 'moon', label: 'Modo Oscuro' },
-    { icon: 'shield-halved', label: 'Políticas de privacidad' },
-    { icon: 'file-lines', label: 'Términos y condiciones' },
-  ];
 
   return (
     <View className='flex-1'>
-      <FlatList
-        data={options}
-        keyExtractor={(item) => item.label}
-        renderItem={({ item }) => <MenuOption icon={item.icon} label={item.label} />}
-        ListHeaderComponent={() => (
-          status === 'unauthenticated' ? (
-            <MenuOption icon='right-to-bracket' label='Iniciar Sesión' onPress={() => router.replace('/auth/login')} />
-          ) : (
-            <>
-              <View className='flex-row w-[90%] items-center p-5 mx-auto bg-white mt-5 justify-between rounded-xl'>
-                <View className='flex-row gap-3 items-center'>
-                  <Icon name='id-card' size={18} color='black' />
-                  <Text className='text-xl'>{user?.usu_name}</Text>
-                </View>
+      <ScrollView className='flex-1'>
+
+        <View className='flex-row w-[90%] items-center p-5 mx-auto bg-white mt-5 justify-between rounded-xl'>
+          <View className='flex-row gap-3 items-center'>
+            <Icon name='id-card' size={18} color='black' />
+            <Text className='text-xl'>{user?.usu_name}</Text>
+          </View>
+          <Icon name='caret-right' size={20} color='black' />
+        </View>
+
+        <View className='bg-white w-[90%] gap-4 mx-auto mt-5 rounded-xl'>
+
+          <Pressable className='flex-row w-full mt-5 items-center p-5 mx-auto bg-white justify-between rounded-xl'>
+            <View className='flex-row gap-3 items-center'>
+              <Icon name='credit-card' size={18} color='black' />
+              <Text className='text-lg'>Plan actual: Gratuito</Text>
+            </View>
+            <Icon name='caret-right' size={20} color='black' />
+          </Pressable>
+
+          <View className='w-[90%] bg-light-primary h-[0.8px] mx-auto' />
+
+          <Pressable className='flex-row w-full items-center p-5 mx-auto bg-white justify-between rounded-xl'>
+            <View className='flex-row gap-3 items-center'>
+              <Icon name='moon' size={18} color='black' />
+              <Text className='text-lg'>Modo Oscuro</Text>
+            </View>
+            <Icon name='caret-right' size={20} color='black' />
+          </Pressable>
+
+          <View className='w-[90%] bg-light-primary h-[0.8px] mx-auto' />
+
+          <Pressable className='flex-row w-full items-center p-5 mx-auto bg-white justify-between rounded-xl'>
+            <View className='flex-row gap-3 items-center'>
+              <Icon name='shield-halved' size={18} color='black' />
+              <Text className='text-lg'>Politicas de privacidad</Text>
+            </View>
+            <Icon name='caret-right' size={20} color='black' />
+          </Pressable>
+
+          <View className='w-[90%] bg-light-primary h-[0.8px] mx-auto' />
+
+          <Pressable className='flex-row w-full items-center p-5 mx-auto bg-white justify-between rounded-xl'>
+            <View className='flex-row gap-3 items-center'>
+              <Icon name='file-lines' size={18} color='black' />
+              <Text className='text-lg'>Terminos y condiciones</Text>
+            </View>
+            <Icon name='caret-right' size={20} color='black' />
+          </Pressable>
+
+          <View className='w-[90%] bg-light-primary h-[0.8px] mx-auto' />
+            <Pressable onPress={logOut} className='flex-row w-full items-center p-5 mx-auto bg-white justify-between rounded-xl'>
+              <View className='flex-row gap-3 items-center'>
+                <Icon name='right-from-bracket' size={18} color='black' />
+                <Text className='text-lg'>Cerrar sesion</Text>
               </View>
-            </>
-          )
-        )}
-        ListFooterComponent={() => (
-          status !== 'unauthenticated' && (
-            <>
-              <MenuOption icon='right-from-bracket' label='Cerrar Sesión' onPress={logOut} />
-              <MenuOption icon='trash' label='Eliminar Cuenta' />
-            </>
-          )
-        )}
-      />
+              <Icon name='caret-right' size={20} color='black' />
+            </Pressable>
+
+            <View className='w-[90%] bg-light-primary h-[0.8px] mx-auto' />
+
+            <Pressable className='flex-row w-full items-center mb-3 p-5 mx-auto bg-white justify-between rounded-xl'>
+              <View className='flex-row gap-3 items-center'>
+                <Icon name='trash' size={18} color='black' />
+                <Text className='text-lg'>Eliminar Cuenta</Text>
+              </View>
+              <Icon name='caret-right' size={20} color='black' />
+            </Pressable>
+
+        </View>
+      </ScrollView>
     </View>
-  );
+  )
 }
